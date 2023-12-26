913ec8ae22957a35949aea6b22a3da8361fa0565
Kittitouch Lelapiyamit <kittitouch.fame@gmail.com>
2023-12-26 10:04:56 +0700

    remove unused device during test process

--- i2c_handler.py
+++ i2c_handler.py
@@ -6,33 +6,25 @@ from utils import *
 from rich import print
 from rich.pretty import pprint
 
-model_require_data_dict = {
-    '2': 2,
-    '3': 1
-}
+model_require_data_dict = {"2": 2, "3": 1}
 
-model_name_dict = {
-    '2': 'Panasonic SN-GCJA5',
-    '3': 'Sensirion SCD4x'
-}
+model_name_dict = {"2": "Panasonic SN-GCJA5", "3": "Sensirion SCD4x"}
 
-model_filename_dict = {
-    '2': 'pana_log.xlsx',
-    '3': 'ss_log.xlsx'
-}
+model_filename_dict = {"2": "pana_log.xlsx", "3": "ss_log.xlsx"}
 
 chart_config_dict = {
-    '2': {
-        'title': 'Panasonic SN-GCJA5 Test Sample',
-        'x_axis': 'Test number',
-        'y_axis': 'PM 2.5 (µg/m³)'
+    "2": {
+        "title": "Panasonic SN-GCJA5 Test Sample",
+        "x_axis": "Test number",
+        "y_axis": "PM 2.5 (µg/m³)",
     },
-    '3': {
-        'title': 'Sensirion SCD40 Test Sample',
-        'x_axis': 'Test number',
-        'y_axis': 'CO2 (ppm)'
-    }
+    "3": {
+        "title": "Sensirion SCD40 Test Sample",
+        "x_axis": "Test number",
+        "y_axis": "CO2 (ppm)",
+    },
 }
+
 
 class I2C_Trinity:
     def __init__(self, bus):
@@ -47,9 +39,9 @@ class I2C_Trinity:
             except OSError as expt:
                 if expt.errno == 16:
                     pass
-        return  devices
-    
-    def read_sensor(self,address):
+        return devices
+
+    def read_sensor(self, address):
         # Interface with sensor
         msg = smbus2.i2c_msg.read(int(address), 27)
         self.bus.i2c_rdwr(msg)
@@ -57,51 +49,61 @@ class I2C_Trinity:
         # data management
         data = bytes(msg)
         data_size = data[0]
-        '''
+        """
         sensirion = Model 1
         Panasonic = Model 2
-        '''
+        """
         model_id = (data[1] << 8) + data[2]
         error_status = (data[5] << 24) + (data[6] << 16) + (data[7] << 8) + data[8]
-        float_data_1 = struct.unpack('!f', bytes(data[11:15]))[0]
-        float_data_2 = struct.unpack('!f', bytes(data[17:21]))[0]
-        float_data_3 = struct.unpack('!f', bytes(data[23:27]))[0]
-        return data_size, model_id, error_status, [float_data_1, float_data_2, float_data_3]
-       
-    
+        float_data_1 = struct.unpack("!f", bytes(data[11:15]))[0]
+        float_data_2 = struct.unpack("!f", bytes(data[17:21]))[0]
+        float_data_3 = struct.unpack("!f", bytes(data[23:27]))[0]
+        return (
+            data_size,
+            model_id,
+            error_status,
+            [float_data_1, float_data_2, float_data_3],
+        )
+
     def start_test(self, round, interval):
         devices = {}
         timestamp = []
         addresses = self.scan()
-        print(f'we found {addresses}')
-        '''
+        print(f"we found {addresses}")
+        """
         Classifiy Sensor Type
-        '''
+        """
         for address in addresses:
-            _, model_id, _, _ = self.read_sensor(address)
+            _, model_id, error_status, _ = self.read_sensor(address)
             model_id = str(model_id)
+            if str(model_id) not in model_name_dict.keys():
+                continue
+            if error_status != 1:
+                continue
             if address not in devices:
                 devices[address] = {}
-                devices[address]['data'] = []
-            devices[address]['model_id'] = model_id
-            
+                devices[address]["data"] = []
+            devices[address]["model_id"] = model_id
+
         # Organize addresses by model IDs
         addresses_by_model = {}
         for address, device_info in devices.items():
-            model_id = device_info['model_id']
+            model_id = device_info["model_id"]
             if model_id not in addresses_by_model:
                 addresses_by_model[model_id] = []
             addresses_by_model[model_id].append(address)
 
         # Print the organized information
         for model_id, model_addresses in addresses_by_model.items():
-            addresses_str = ', '.join(map(str, model_addresses))
-            print(f"[green]{model_name_dict[model_id]}[/green] has addresses: [purple]{addresses_str}[purple]")
+            addresses_str = ", ".join(map(str, model_addresses))
+            print(
+                f"[green]{model_name_dict[model_id]}[/green] has addresses: [purple]{addresses_str}[purple]"
+            )
 
         for cnt in range(round):
             print()
             print(f"[bold cyan] Round [/bold cyan] : [green]{cnt}[/green]")
-            print(f'[green]{get_human_datetime()}[/green]')
+            print(f"[green]{get_human_datetime()}[/green]")
             print()
             timestamp.append(get_datetime())
             start_time = time.time()
@@ -109,30 +111,39 @@ class I2C_Trinity:
                 try:
                     _, model_id, _, packet_data = self.read_sensor(address)
                     model_id = str(model_id)
-                    
+
                     targeted_data = packet_data[model_require_data_dict[model_id]]
-                    devices[address]['data'].append(targeted_data)
-                    print(f'[bold magenta]Address [/bold magenta] : [orchid]{address}[orchid], [bold light_coral]Type[/bold light_coral] : [salmon1]{model_name_dict[model_id]}[/salmon1], [bold cyan]Data[/bold cyan] : [blue]{targeted_data}[blue]')
+                    devices[address]["data"].append(targeted_data)
+                    print(
+                        f"[bold magenta]Address [/bold magenta] : [orchid]{address}[orchid], [bold light_coral]Type[/bold light_coral] : [salmon1]{model_name_dict[model_id]}[/salmon1], [bold cyan]Data[/bold cyan] : [blue]{targeted_data}[blue]"
+                    )
                     # devices['datetime'].append(get_datetime())
                 except Exception as error:
-                    devices[address]['data'].append(0)
-                    print(f"[red] Can't read data on address [/red] : [bold red]{address}[/bold red]")
+                    devices[address]["data"].append(0)
+                    print(
+                        f"[red] Can't read data on address [/red] : [bold red]{address}[/bold red]"
+                    )
                     print("An exception occurred:", error)
 
                 time.sleep(0.1)
-        
-            while (time.time() - start_time < interval):
-                if (cnt == (round-1)):
+
+            while time.time() - start_time < interval:
+                if cnt == (round - 1):
                     break
 
         sensors_group = format_data_to_excel(devices, timestamp)
-        
+
         for model_id in sensors_group.keys():
-            file_name =  os.path.join(pathlib.Path(__file__).parent.resolve(),'excel',model_filename_dict[model_id])
-            create_excel(file_name,sensors_group[model_id],round, chart_config_dict[model_id])
-            
-if __name__ == '__main__':
+            file_name = os.path.join(
+                pathlib.Path(__file__).parent.resolve(),
+                "excel",
+                model_filename_dict[model_id],
+            )
+            create_excel(
+                file_name, sensors_group[model_id], round, chart_config_dict[model_id]
+            )
+
+
+if __name__ == "__main__":
     i2c_handler = I2C_Trinity(1)
     print(I2C_Trinity.scan())
-
-
